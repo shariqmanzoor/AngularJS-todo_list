b27d52745547678036d367f7af4407515b1a7c60
Shariq Manzoor <shariqmanzoor6@gmail.com>
2020-02-29 12:27:23 +0500

    functionality & project complete

--- script.js
+++ script.js
@@ -2,4 +2,18 @@ var mod = angular.module('todo_mod', []);
 
 mod.controller('todo_cont', function($scope) {
     $scope.list = ["Task1", "Task2", "Task3", "Task4"];
+
+    $scope.new = "";
+
+    $scope.addItem = function(){
+
+      $scope.list.push($scope.new);
+      $scope.new = "";
+    }
+
+    $scope.delete = function(index) {
+
+    $scope.list.splice(index, 1);
+
+    }
 });
